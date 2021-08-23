fc540469a2b03e19e1a629771c1cb1f4ce7f5c2d
Zaki Sandhu <77785571+zakisandhu@users.noreply.github.com>
2021-08-23 17:56:52 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,33 +3,33 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint = Matter.Constraint;
 
-function preload()
-{
-	
-}
+// add the required varaibles
+var world, engine;
+var ball1, ball2, ball3, ball4, ball5;
+var roof;
 
-function setup() {
-	createCanvas(800, 700);
+function setup(){
+    
+    //create a canvas to display the objects
+    createCanvas(800, 600);
+    rectMode(CENTER);
 
+    // create a engine and a world
+    engine = Engine.create();
+    world = engine.world;
 
-	engine = Engine.create();
-	world = engine.world;
+    // add the balls
+    ball1 = new Newton_Ball(320,575,40);
+	ball2 = new Newton_Ball(360,575,40);
+	ball3 = new Newton_Ball(400,575,40);
+	ball4 = new Newton_Ball(440,575,40);
+	ball5 = new Newton_Ball(480,575,40);    
 
-	//Create the Bodies Here.
+    
 
-
-	Engine.run(engine);
-  
-}
-
-
-function draw() {
-  rectMode(CENTER);
-  background(0);
-  
-  drawSprites();
- 
 }
 
 
